3133cd7abeb2b93041054bb8e06d33384e98ad24
maikully <michael_s_li@brown.edu>
2020-06-10 17:03:12 -0400

    changed spacing and alignments

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,31 +1,38 @@
 import React, { Component } from "react";
 
 class Contact extends Component {
-	render() {
-		if (this.props.data) {
-			var message = this.props.data.contactmessage;
-		}
-		
-		const styleWhite = {color : 'white'};
-		return (
-			<section id="contact">
-				<div className="row section-head">
-					<div className="ten columns">
-						<h2 style={styleWhite}> Interested in becoming a volunteer developer? </h2>
-					</div>
-					
-					<div className="two columns header-col">
-					</div>
-					<div className="ten columns">
-						<p className="gray">{message}</p>
-					</div>
-					<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeoKjpBpz5Wq8aTRxoUqoOmmbj3Ly6wqwp0YlvwlaKyqLJ25w/viewform?embedded=true" width="640" height="524" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+  render() {
+    if (this.props.data) {
+      var message = this.props.data.contactmessage;
+    }
 
-				</div>
-
-			</section>
-		);
-	}
+    const styleWhite = { color: "white", text0align: "center" };
+    return (
+      <section id="contact">
+        <h2 style={styleWhite}>
+          Interested in becoming a volunteer developer?
+        </h2>
+        <div className="row banner">
+          <div className="banner-text">
+            <div className="two columns header-col"></div>
+            <div className="ten columns">
+              <p className="gray">{message}</p>
+              <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLSfzsY9hhopa3cQlb2efHzK0ZTIPOAqtkN8H1ug9-BE3Otv4hQ/viewform"
+                width="640"
+                height="524"
+                frameborder="0"
+                marginheight="0"
+                marginwidth="0"
+              >
+                Loading…
+              </iframe>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Contact;
