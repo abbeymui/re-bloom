9782a22a17b8242b4b8b07a87226c3ed25cfa0b2
maikully <michael_s_li@brown.edu>
2020-06-10 15:20:41 -0400

    Add google form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,9 +3,6 @@ import React, { Component } from "react";
 class Contact extends Component {
   render() {
     if (this.props.data) {
-      var name = this.props.data.name;
-      var phone = this.props.data.phone;
-      var email = this.props.data.email;
       var message = this.props.data.contactmessage;
     }
 
@@ -16,98 +13,14 @@ class Contact extends Component {
             <h1>
               <span>Get In Touch.</span>
             </h1>
-          </div>
-
+          </div>        
           <div className="ten columns">
             <p className="lead">{message}</p>
           </div>
+		  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeoKjpBpz5Wq8aTRxoUqoOmmbj3Ly6wqwp0YlvwlaKyqLJ25w/viewform?embedded=true" width="640" height="524" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
         </div>
-
-        <div className="row">
-          <div className="eight columns">
-            <form action="" method="post" id="contactForm" name="contactForm">
-              <fieldset>
-                <div>
-                  <label htmlFor="contactName">
-                    Name <span className="required">*</span>
-                  </label>
-                  <input
-                    type="text"
-                    defaultValue=""
-                    size="35"
-                    id="contactName"
-                    name="contactName"
-                    onChange={this.handleChange}
-                  />
-                </div>
-
-                <div>
-                  <label htmlFor="contactEmail">
-                    Email <span className="required">*</span>
-                  </label>
-                  <input
-                    type="text"
-                    defaultValue=""
-                    size="35"
-                    id="contactEmail"
-                    name="contactEmail"
-                    onChange={this.handleChange}
-                  />
-                </div>
-
-                <div>
-                  <label htmlFor="contactSubject">Subject</label>
-                  <input
-                    type="text"
-                    defaultValue=""
-                    size="35"
-                    id="contactSubject"
-                    name="contactSubject"
-                    onChange={this.handleChange}
-                  />
-                </div>
-
-                <div>
-                  <label htmlFor="contactMessage">
-                    Message <span className="required">*</span>
-                  </label>
-                  <textarea
-                    cols="50"
-                    rows="15"
-                    id="contactMessage"
-                    name="contactMessage"
-                  ></textarea>
-                </div>
-
-                <div>
-                  <button className="submit">Submit</button>
-                  <span id="image-loader">
-                    <img alt="" src="images/loader.gif" />
-                  </span>
-                </div>
-              </fieldset>
-            </form>
-
-            <div id="message-warning"> Error boy</div>
-            <div id="message-success">
-              <i className="fa fa-check"></i>Your message was sent, thank you!
-              <br />
-            </div>
-          </div>
-
-          <aside className="four columns footer-widgets">
-            <div className="widget widget_contact">
-              <h4>Email and Phone</h4>
-              <p className="address">
-                {name}
-                <br />
-                {email}
-                <br />
-                <span>{phone}</span>
-              </p>
-            </div>
-          </aside>
-        </div>
+		
       </section>
     );
   }
