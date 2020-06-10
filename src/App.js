260bbeb71c17150880754e288132a78ffec5fe0d
Melodee Li <melodeeli98@gmail.com>
2020-06-10 17:33:08 -0400

    content updates and ui changes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import "./App.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import About from "./Components/About";
-import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
-import Testimonials from "./Components/Testimonials";
+//import Testimonials from "./Components/Testimonials";
 import Portfolio from "./Components/Portfolio";
-import resumeData from "./Data/resumeData.json";
+import data from "./Data/data.json";
 
 class App extends Component {
   constructor(props) {
@@ -20,13 +19,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header data={resumeData.main} />
-        <About data={resumeData.main} />
-        <Resume data={resumeData.resume} />
-        <Portfolio data={resumeData.portfolio} />
-        <Testimonials data={resumeData.testimonials} />
-        <Contact data={resumeData.main} />
-        <Footer data={resumeData.main} />
+        <Header data={data.main} />
+        <About data={data.main} />
+        <Portfolio data={data.portfolio} />
+        {/*<Testimonials data={data.testimonials} />*/}
+        <Contact data={data.main} />
+        <Footer data={data.main} />
       </div>
     );
   }
