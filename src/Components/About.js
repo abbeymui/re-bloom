6dc1699505f7efef7557ea92670aabd1739a5309
Melodee Li <melodeeli98@gmail.com>
2020-06-26 17:38:42 -0400

    update contacts

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,10 +6,10 @@ class About extends Component {
       var logo = this.props.data.logo;
       var name = this.props.data.name;
       var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
+      //var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
+      //var zip = this.props.data.address.zip;
       var phone = this.props.data.phone;
       var email = this.props.data.email;
     }
@@ -29,11 +29,10 @@ class About extends Component {
                 <h2>Contact Details</h2>
                 <p className="address">
                   <span>{name}</span>
-                  <br />
                   <span>
-                    {street}
+                    {/*street*/}
                     <br />
-                    {city} {state}, {zip}
+                    {city} {state}{/*}, {zip}*/}
                   </span>
                   <br />
                   <span>{phone}</span>
